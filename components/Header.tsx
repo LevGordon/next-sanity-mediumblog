13ef351f5cdcd9cec365a1fa590d96d190daf88c
Lev_Gordon <lev.gordonsa@gmail.com>
2022-11-30 16:46:47 -0800

    banner tsx and tailwind

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
     <header className="flex justify-between p-5 max-w-7xl mx-auto">
         <div className="flex items-center space-x-5">
             <Link href='/' >
-                <img className="w-44 object-contain cursor-pointer" src="" alt="blog-logo" />
+                <img className="w-44 object-contain cursor-pointer" src="https://links.papareact.com/yvf" alt="blog-logo" />
             </Link>
             <div className="hidden md:inline-flex items-center space-x-5">
             <h3>About</h3>
