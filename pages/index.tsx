7b127519e5e13bcb80ed780a282c0f4115aa7af7
Lev_Gordon <lev.gordonsa@gmail.com>
2022-12-02 15:38:16 -0800

    sanity CMS connected, posts displaying in console

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../typings'
 
-const Home: NextPage = () => {
+
+interface Props {
+  posts: [Post]
+}
+
+export default function Home({ posts }: Props) {
+  console.log("posts:", posts)
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -19,4 +25,25 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == 'post'] {
+    _id,
+    title,
+    author -> {
+    name,
+    image
+  },
+    description,
+    mainImage,
+    slug
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
