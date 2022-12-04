0cf7ffce13e13590c457658d7a57c389385150b7
Lev_Gordon <lev.gordonsa@gmail.com>
2022-12-03 21:03:25 -0800

    post renders from sanity data. Body text types asigned. Query bug fix

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from 'next'
 import Header from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../../typings'
+import PortableText from 'react-portable-text'
 
 interface Props {
     post: Post;
@@ -9,10 +10,48 @@ interface Props {
 
 
 function Post({ post } : Props) {
-    console.log("POST:", post)
   return (
     <main>
         <Header />
+
+        <img className='w-full h-40 object-cover' src={urlFor(post.mainImage).url()!} alt={post.title} />
+
+        <article className='max-w-3xl mx-auto p-5'>
+            <h1 className='text-3xl mt-10 mb-3'>{post.title}</h1>
+            <h2 className='text-xl font-light text-gray-500 mb-2'>{post.description}</h2>
+
+            <div className='flex items-center space-x-2'>
+                <img className='h-10 w-10 rounded-full' src={urlFor(post.author.image).url()!} alt={post.author.name} />
+                <p className='font-extralight text-sm'>Blog post by: <span className='text-green-600'>{post.author.name}</span></p>
+                <p className='font-extralight text-sm'>Published at {new Date(post._createdAt).toLocaleString()}</p>
+            </div>
+
+            <div className='mt-10'>
+                <PortableText 
+                className=''
+                dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+                projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+                content={post.body}
+                serializers={{
+                    h1: (props: any) => (
+                        <h1 className='text-2xl font-bold my-5' {...props}/>
+                    ),
+                    h2: (props: any) => (
+                        <h2 className='text-xl font-bold my-5' {...props}/>
+                    ),
+                    li: ({children}: any) => (
+                        <li className='ml-4 list-disc'> {children}</li>
+                    ),
+                    link: ({href, children}: any) => (
+                        <a href={href} className='text-blue-500 hover:underline'>{children}</a>
+                    )
+                }}
+                
+                />
+            </div>
+
+
+        </article>
     </main>
   )
 }
@@ -21,7 +60,7 @@ export default Post
 
 
 export const getStaticPaths = async () => {
-    const query = `*[_type == 'post'] {
+    const query = `*[_type == "post"] {
         _id,
         slug {
             current
@@ -44,7 +83,7 @@ export const getStaticPaths = async () => {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-    const query = `*[_type == "post" && slug.current == "my-first-post"][0] {
+    const query = `*[_type == "post" && slug.current == $slug][0] {
         _id,
         _createdAt,
         title,
