a6a35aab96343862684bab1a52915ee87c20d04b
Lev_Gordon <lev.gordonsa@gmail.com>
2022-12-06 19:21:26 -0800

    submitHandler logic

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -20,6 +20,20 @@ function Post({ post }: Props) {
 
   const { register, handleSubmit, formState: {errors} } = useForm<FormInput>()
 
+
+
+  const onSubmit: SubmitHandler<FormInput> = async (data) => {
+    await fetch('/api/createComment', {
+      method: "POST",
+      body: JSON.stringify(data)
+    }).then(() => {
+      console.log(data)
+    }).catch((error) => {
+      console.log(error)
+    })
+
+  }
+
   return (
     <main>
       <Header />
@@ -79,7 +93,7 @@ function Post({ post }: Props) {
 
         <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
 
-        <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
             <h3 className="text-sm text-yellow-500"> Enjoyed this article?</h3>
             <h4 className="text-3xl font-bold"> Leave a comment below!</h4>
             <hr className="py-3 mt-2" />
@@ -99,7 +113,7 @@ function Post({ post }: Props) {
             </label>
             <label className="block mb-5">
                 <span className="text-gray-7000">Email</span>
-                <input {...register("email", { required: true })} className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring" type="text" placeholder="john.appleseed@email.com" />    
+                <input {...register("email", { required: true })} className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring" type="email" placeholder="john.appleseed@email.com" />    
             </label>
             <label className="block mb-5">
                 <span className="text-gray-7000">Comment</span>
